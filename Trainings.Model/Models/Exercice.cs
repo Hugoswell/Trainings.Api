de2159f50e2e5839a95ee4c97e8cf96474d5ce04
Hugo Oswell <hugo.oswell@gmail.com>
2020-02-18 19:18:18 +0100

    Finish to scaffold DB context and clean models objects

---
+++ Trainings.Model/Models/Exercice.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Trainings.Model.Models
+{
+    public partial class Exercice
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int TrainingId { get; set; }
+
+        public short ExerciceId { get; set; }
+
+        public byte? NbOfSets { get; set; }
+
+        public byte? NbOfRepetitions { get; set; }
+
+        public double? Duration { get; set; }
+
+        public double RestDuration { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string MuscleGroup { get; set; }
+
+        [ForeignKey(nameof(ExerciceId))]
+        [InverseProperty("ExerciceLov")]
+        public virtual Lov Lov { get; set; }
+
+        [ForeignKey(nameof(TrainingId))]
+        [InverseProperty("ExerciceTraining")]
+        public virtual Training Training { get; set; }
+    }
+}
---
+++ Trainings.Model/Models/Training.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Trainings.Model.Models
+{
+    public partial class Training
+    {
+        public Training()
+        {
+            Exercices = new HashSet<Exercice>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public int UserPreferenceId { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime CreationDate { get; set; }
+
+        public byte NbOfExercice { get; set; }
+
+        public byte Duration { get; set; }
+
+        [ForeignKey(nameof(UserPreferenceId))]
+        [InverseProperty("TrainingUserPreference")]
+        public virtual UserPreference UserPreference { get; set; }
+
+        [InverseProperty("TrainingExercice")]
+        public virtual ICollection<Exercice> Exercices { get; set; }
+    }
+}
