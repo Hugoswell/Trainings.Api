a1cd31cd8960df8f27056476d79c350265a604b3
Hugo Oswell <hugo.oswell@gmail.com>
2020-02-19 23:00:40 +0100

    Forward on register business layer

---
+++ Trainings.Business/Interface/IRegisterBusiness.cs
@@ -0,0 +1,7 @@
+﻿namespace Trainings.Business.Interface
+{
+    interface IRegisterBusiness
+    {
+
+    }
+}
---
+++ Trainings.Business/RegisterBusiness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Trainings.Business.Interface;
+using Trainings.Repository.Interface;
+
+namespace Trainings.Business
+{
+    public class RegisterBusiness : IRegisterBusiness
+    {
+        #region Constructor & Properties
+
+        private readonly IRegisterRepository _registerRepository;
+
+        public RegisterBusiness(IRegisterRepository registerRepository)
+        {
+            _registerRepository = registerRepository ?? throw new ArgumentNullException(nameof(registerRepository));
+        }
+
+        #endregion
+
+        
+    }
+}
