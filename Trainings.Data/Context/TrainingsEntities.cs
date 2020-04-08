2157b916820cc33f55767d7530b20eae906fa9ad
Hugo Oswell <hugo.oswell@gmail.com>
2020-04-08 12:37:56 +0200

    Remove connection string

--- Trainings.Data/Context/TrainingsEntities.cs
+++ Trainings.Data/Context/TrainingsEntities.cs
@@ -31,16 +31,7 @@ namespace Trainings.Data.Context
         public virtual DbSet<UserPhysicalInformation> UserPhysicalInformation { get; set; }
         public virtual DbSet<UserPreferences> UserPreferences { get; set; }
         public virtual DbSet<UserTrainingFrequency> UserTrainingFrequency { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=Trainings;Trusted_Connection=True;");
-            }
-        }
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Equipment>(entity =>
