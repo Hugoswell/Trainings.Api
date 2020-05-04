edd19fd4777da9e5c8402ac9126bd7340dd906f6
Hugo Oswell <hugo.oswell@gmail.com>
2020-05-04 20:03:54 +0200

    Forward on sql information for user

---
+++ Trainings.Sql/03-InsertData.sql
@@ -0,0 +1,38 @@
+USE[Trainings]
+GO
+
+-- Goal
+INSERT INTO dbo.Goal VALUES (1, 'WeightLoss')
+INSERT INTO dbo.Goal VALUES (2, 'WeightGain')
+INSERT INTO dbo.Goal VALUES (3, 'Explosiveness')
+INSERT INTO dbo.Goal VALUES (4, 'Strength')
+
+-- TrainingType
+INSERT INTO dbo.TrainingType VALUES (1, 'BodyBuilding')
+INSERT INTO dbo.TrainingType VALUES (2, 'Cardio')
+INSERT INTO dbo.TrainingType VALUES (3, 'Crossfit')
+INSERT INTO dbo.TrainingType VALUES (4, 'HIIT')
+
+-- TrainingDuration
+INSERT INTO dbo.TrainingDuration VALUES (1, '15-30')
+INSERT INTO dbo.TrainingDuration VALUES (2, '30-45')
+INSERT INTO dbo.TrainingDuration VALUES (3, '45-1')
+INSERT INTO dbo.TrainingDuration VALUES (4, '1-2')
+
+-- Equipment
+INSERT INTO dbo.Equipment VALUES (1, 'BodyWeight')
+INSERT INTO dbo.Equipment VALUES (2, 'Gym')
+INSERT INTO dbo.Equipment VALUES (3, 'HomeMaterial')
+
+-- UserLevel
+SET IDENTITY_INSERT Trainings.dbo.UserLevel ON
+INSERT INTO dbo.UserLevel (Id, [Name]) VALUES (1, 'Beginner')
+INSERT INTO dbo.UserLevel (Id, [Name]) VALUES (2, 'Confirmed')
+INSERT INTO dbo.UserLevel (Id, [Name]) VALUES (3, 'Expert')
+SET IDENTITY_INSERT Trainings.dbo.UserLevel OFF
+
+-- UserSex
+SET IDENTITY_INSERT Trainings.dbo.UserSex ON
+INSERT INTO dbo.UserSex (Id, [Name]) VALUES (1, 'Man')
+INSERT INTO dbo.UserSex (Id, [Name]) VALUES (2, 'Woman')
+SET IDENTITY_INSERT Trainings.dbo.UserSex OFF
