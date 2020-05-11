53aa7aaf385fea58a019ea1ef9289db7b5b60095
Hugo Oswell <hugo.oswell@gmail.com>
2020-05-11 19:27:23 +0200

    Forward on SQL exercices library

---
+++ Trainings.Sql/04-ExerciceLibrary.sql
@@ -0,0 +1,107 @@
+SET IDENTITY_INSERT dbo.Exercice ON
+-- Musculation
+-- Shoulders
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (1, 'Développé militaire assis au cadre guidé', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (2, 'Tirage menton debout avec barre', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (3, 'Développé arnold assis', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (4, 'Elévation latérale assis avec haltères', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (5, 'Elévation antérieure debout avec haltères', '')
+
+-- Pectoral
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (6, 'Développé couché avec barre', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (7, 'Développé couché avec haltères', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (8, 'Développé incliné avec barre', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (9, 'Développé incliné avec haltères', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (10, 'Pec Deck', '')
+
+-- Back
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (11, 'Tractions pronation', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (12, 'Tractions supination', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (13, 'Tirage poulie haute pronation', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (14, 'Tirage horizontal assis prise neutre', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (15, 'Rowing buste penché avec barre en pronation', '')
+
+-- Arms
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (16, 'Curl debout avec barre en supination', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (17, 'Curl assis avec haltères en supination', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (18, 'Curl assis avec haltères en prise neutre', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (19, 'Dips', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (20, 'Extension avec barre au front en pronation', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (21, 'Extension assis un haltère à deux bras', '')
+
+-- Abs
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (22, 'Flexion de buste à la poulie haute', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (23, 'Relevé de buste au sol pieds sur banc', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (24, 'Relevé de buste sur planche décliné', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (25, 'Oblique debout avec poulie basse', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (26, 'Essuie-glace à la barre fixe', '')
+
+-- Legs
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (27, 'Squat à la barre', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (28, 'Squat au cadre guidé', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (29, 'Squat militaire avec barre', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (30, 'Fentes avec haltères', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (31, 'Leg Press', '')
+
+-- Legs
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (32, 'Montée sur banc avec haltères', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (33, 'Extension une jambe debout à la poulie basse', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (34, 'Presse inclinée avec talons uniquement', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (35, 'Hip thrust', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (36, 'Good morning', '')
+
+
+
+-- Cardio
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (37, 'Course à pieds en extérieur', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (38, 'Course à pieds sur tapis', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (39, 'Vélo elliptique', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (40, 'Yoga', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (41, 'Marche rapide en extérieur', '')
+
+
+
+-- Crossfit / HIIT
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (42, 'Gorille', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (43, 'Boxe coup droit', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (44, 'Squat Jump', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (45, 'Essuie glace debout', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (46, 'Squat sumo', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (47, 'Genoux twist', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (48, 'Fentes sautées', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (49, 'Burpees', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (50, 'Soulevé de terre 1 jambe', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (51, 'Talons fesses', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (52, 'Ciseaux devant derrières', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (53, 'Montée de genoux', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (54, 'Tipping', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (55, 'Pompes', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (56, 'Coup de genoux debout', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (57, 'Gainage grimpeur', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (58, 'Pompes scorpion', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (59, 'Gainage rotation bras en l''air alterné', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (60, 'Uppercut', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (61, 'Gainage oblique', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (62, 'Tire corde buste penché', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (63, 'Jumping Jacks', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (64, 'Fentes latérales', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (65, 'Spider climbers', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (66, 'Montée de hanches alternées au sol', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (67, 'Patineur', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (68, 'Essuie-glace assis', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (69, 'Corde à sauter', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (70, 'Sauts latéraux', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (71, 'Montée de corde', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (72, 'Superman', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (73, 'Chaise romaine', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (74, 'Crabe kicks', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (75, 'Planche saut groupé', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (76, 'Bateau', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (77, 'Bicyclette', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (78, 'Pompes chassées', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (79, 'Pompes croco', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (80, 'Bateau statique', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (81, 'Pompes en marchant', '')
+INSERT INTO dbo.Exercice (Id, Name, Description)  VALUES (82, 'Fentes arrières alternées ', '')
+
+SET IDENTITY_INSERT dbo.Exercice OFF
